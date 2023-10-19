e8ae9d078f51ae6423a9ef7733fc91c6d023d63e
Jazm1nP <141263906+Jazm1nP@users.noreply.github.com>
2023-10-19 23:27:04 +0000

    FLW Oct 19

--- operators.js
+++ operators.js
@@ -3,26 +3,33 @@
 
 // 1. Addition
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
+let num1= 22
+let num2= 11
 // Add the two numbers and store the result in a new variable called sum.
+let sum= num1+num2
 // Print the result using console.log().
-
+console.log (sum)
 
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
-
-
+let difference= num1-num2
+console.log (difference)
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
-
-
+let product= num1*num2
+console.log (product)
 
 // String Operations
 
 // 4. Concatenation with +
 // Create two string variables, str1 and str2, and assign them any two strings of your choice.
+let str1= "ice"
+let str2= "cream"
+
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
-
+let combinedString= str1+ str2
+console.log ()
--- variables.js
+++ variables.js
@@ -1,14 +1,17 @@
 // 1. Create a variable called `favoriteColor` and assign it a string value of your favorite color.
-let favoriteColor = "YOUR_COLOR_HERE";
+let favoriteColor = "purple";
+let favoriteFood="tacos"
+  //create a variable called `favoriteFood` and assign it a string value of your favorite food.
 
-// 2. Create a variable called `favoriteFood` and assign it a string value of your favorite food.
-
-
+let favoriteNumber= 4
 // 3. Create a variable called `favoriteNumber` and assign it a number value of your favorite number.
 
-
+let itsSunnyToday= false
 // 4. Create a variable called `isSunnyToday` and assign it a boolean value based on if it's sunny today.
 
 
 // 5. Console log each variable on a new line.
-
+console.log (favoriteColor)
+console.log (favoriteFood)
+console.log (favoriteNumber)
+console.log (itsSunnyToday)
